f595f81e78cd03aead2891382242ae56527cda9b
Никита Сандрейкин <ikusei.keikaku.163@gmail.com>
2020-10-26 13:46:16 +0400

    Добавьте файлы проекта.

---
+++ Lab3_v22/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Lab3_v22
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(139, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 123);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(154, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Деньги клиента:";
+            this.label1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(300, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Добавить клиента";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Номера за 200 рублей:";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Номера за 400 рублей:";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 295);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Номера за 600 рублей:";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Занято:";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(125, 154);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 17);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Всего номеров:";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 248);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 17);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Занято:";
+            this.label7.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(125, 248);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(109, 17);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Всего номеров:";
+            this.label8.Visible = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 344);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 17);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Занято:";
+            this.label9.Visible = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(125, 344);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(109, 17);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "Всего номеров:";
+            this.label10.Visible = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(88, 154);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(21, 22);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "0";
+            this.label11.Visible = false;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(88, 248);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(21, 22);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "0";
+            this.label12.Visible = false;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.Location = new System.Drawing.Point(88, 344);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(21, 22);
+            this.label13.TabIndex = 4;
+            this.label13.Text = "0";
+            this.label13.Visible = false;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label14.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label14.Location = new System.Drawing.Point(256, 154);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(21, 22);
+            this.label14.TabIndex = 4;
+            this.label14.Text = "3";
+            this.label14.Visible = false;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label15.Location = new System.Drawing.Point(256, 248);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(21, 22);
+            this.label15.TabIndex = 4;
+            this.label15.Text = "5";
+            this.label15.Visible = false;
+            this.label15.Click += new System.EventHandler(this.Label15_Click);
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label16.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label16.Location = new System.Drawing.Point(256, 344);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(21, 22);
+            this.label16.TabIndex = 4;
+            this.label16.Text = "3";
+            this.label16.Visible = false;
+            this.label16.Click += new System.EventHandler(this.Label16_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 393);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Гостиница";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        public System.Windows.Forms.Label label11;
+        public System.Windows.Forms.Label label12;
+        public System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Lab3_v22/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace Lab3_v22
+{
+    public partial class Form1 : Form
+    {
+        List<Посетитель> room_200;
+        List<Посетитель> room_400;
+        List<Посетитель> room_600;
+        List<Посетитель> incoming;
+        BackgroundWorker _200;
+        BackgroundWorker _400;
+        BackgroundWorker _600;
+        BackgroundWorker waiters;
+        BackgroundWorker main;
+        public static bool one = false;
+        public static bool two = false;
+        public static bool three = false;
+        public static int i = 0;
+        public static int count_200 = 0;
+        public static int count_400 = 0;
+        public static int count_600 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        async void main_f()
+        {
+            while (true)
+            {
+                Action action = () =>
+                {
+                    label11.Text = count_200.ToString();
+                    label12.Text = count_400.ToString();
+                    label13.Text = count_600.ToString();
+                };
+                Invoke(action);
+                Thread.Sleep(50);
+            }
+        }
+        async void incoming_f()
+        {
+            while (true)
+            {
+                for (int j = 0; j < incoming.Count; j++)
+                {
+                    Action action = () =>
+                    {
+                        if (incoming[j].n >= 600)
+                        {
+                            if (room_600.Count < 3)
+                            {
+                                room_600.Add(incoming[j]);
+                                count_600++;
+                                one = true;
+                                incoming.RemoveAt(j);
+                            }
+                            else if (room_400.Count < 5)
+                            {
+                                room_400.Add(incoming[j]);
+                                count_400++;
+                                two = true;
+                                incoming.RemoveAt(j);
+                            }
+                            else if (room_200.Count < 3)
+                            {
+                                room_200.Add(incoming[j]);
+                                count_200++;
+                                three = true;
+                                incoming.RemoveAt(j);
+                            }
+                        }
+                        else if (incoming[j].n >= 400)
+                        {
+                            if (room_400.Count < 5)
+                            {
+                                room_400.Add(incoming[j]);
+                                count_400++;
+                                two = true;
+                                incoming.RemoveAt(j);
+                            }
+                            else if (room_200.Count < 3)
+                            {
+                                room_200.Add(incoming[j]);
+                                count_200++;
+                                three = true;
+                                incoming.RemoveAt(j);
+                            }
+                        }
+                        else
+                        {
+                            if (room_200.Count < 3)
+                            {
+                                room_200.Add(incoming[j]);
+                                count_200++;
+                                three = true;
+                                incoming.RemoveAt(j);
+                            }
+                        }
+                    };
+                    Invoke(action);
+                    Thread.Sleep(50);
+                }
+            }
+        }
+        async void using_200(object a)
+        {
+            Посетитель temp = (Посетитель)a;
+            Action action = () =>
+            {
+                temp.label6.Text = "В номере за 200 рублей";
+            };
+            Invoke(action);
+            Thread.Sleep(10000);
+            Action action2 = () =>
+            {
+                temp.Close();
+                count_200--;
+                room_200.Remove(temp);
+            };
+            Invoke(action2);
+        }
+        async void using_400(object a)
+        {
+            Посетитель temp = (Посетитель)a;
+            Action action = () =>
+            {
+                temp.label6.Text = "В номере за 400 рублей";
+            };
+            Invoke(action);
+            Thread.Sleep(10000);
+            Action action2 = () =>
+            {
+                temp.Close();
+                count_400--;
+                room_400.Remove(temp);
+            };
+            Invoke(action2);
+        }
+        async void using_600(object a)
+        {
+            Посетитель temp = (Посетитель)a;
+            Action action = () =>
+            {
+                temp.label6.Text = "В номере за 600 рублей";
+            };
+            Invoke(action);
+            Thread.Sleep(10000);
+            Action action2 = () =>
+            {
+                temp.Close();
+                count_600--;
+                room_600.Remove(temp);
+            };
+            Invoke(action2);
+        }
+        async void _200_f()
+        {
+            while (true)
+            {
+                if (three)
+                {
+                    for (int j = 0; j < room_200.Count(); j++)
+                    {
+                        if (room_200[j].check)
+                        {
+                            Thread temp = new Thread(new ParameterizedThreadStart(using_200));
+                            temp.Start(room_200[j]);
+                            room_200[j].check = false;
+                            Thread.Sleep(50);
+                        }
+                    }
+                    three = false;
+                }
+            }
+        }
+        async void _400_f()
+        {
+            while (true)
+            {
+                if (two)
+                {
+                    for (int j = 0; j < room_400.Count(); j++)
+                    {
+                        if (room_400[j].check)
+                        {
+                            Thread temp = new Thread(new ParameterizedThreadStart(using_400));
+                            temp.Start(room_400[j]);
+                            room_400[j].check = false;
+                            Thread.Sleep(50);
+                        }
+                    }
+                    two = false;
+                }
+            }
+        }
+        async void _600_f()
+        {
+            while (true)
+            {
+                if (one)
+                {
+                    for (int j = 0; j < room_600.Count(); j++)
+                    {
+                        if (room_600[j].check)
+                        {
+                            Thread temp = new Thread(new ParameterizedThreadStart(using_600));
+                            temp.Start(room_600[j]);
+                            room_600[j].check = false;
+                            Thread.Sleep(50);
+                        }
+                    }
+                    one = false;
+                }
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true; label2.Visible = true; label3.Visible = true; label4.Visible = true;
+            label5.Visible = true; label6.Visible = true; label7.Visible = true; label8.Visible = true;
+            label9.Visible = true; label10.Visible = true; label11.Visible = true; label12.Visible = true;
+            label13.Visible = true; label14.Visible = true; label15.Visible = true; label16.Visible = true;
+            textBox1.Visible = true; button2.Visible = true; button1.Visible = false;
+            room_600 = new List<Посетитель>();
+            room_400 = new List<Посетитель>();
+            room_200 = new List<Посетитель>();
+            incoming = new List<Посетитель>();
+            main = new BackgroundWorker();
+            main.DoWork += (obj, ea) => main_f();
+            main.RunWorkerAsync();
+            _200 = new BackgroundWorker();
+            _200.DoWork += (obj, ea) => _200_f();
+            _200.RunWorkerAsync();
+            _400 = new BackgroundWorker();
+            _400.DoWork += (obj, ea) => _400_f();
+            _400.RunWorkerAsync();
+            _600 = new BackgroundWorker();
+            _600.DoWork += (obj, ea) => _600_f();
+            _600.RunWorkerAsync();
+            waiters = new BackgroundWorker();
+            waiters.DoWork += (obj, ea) => incoming_f();
+            waiters.RunWorkerAsync();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            i++;
+            int n = Convert.ToInt32(textBox1.Text);
+            if (n < 200) { MessageBox.Show("У посетителя слишком мало денег", "Ошибка", MessageBoxButtons.OK); }
+            else
+            {
+                Посетитель temp = new Посетитель();
+                temp.check = true;
+                temp.n = n;
+                incoming.Add(temp);
+                temp.label4.Text = i.ToString();
+                temp.label5.Text = n.ToString();
+                temp.Show();
+            }
+        }
+    }
+}
